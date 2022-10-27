44f4c2edfc976d5846cef1799e2fe92aaaa7c2de
ZOE <43523594+2625792753@users.noreply.github.com>
2022-10-27 19:08:08 +0800

    server

--- src/info_server.java
+++ src/info_server.java
@@ -1,71 +1,56 @@
-
-import javafx.util.Pair;
-
+import
 import java.io.IOException;
-
 import java.net.*;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class info_server {
-    private static final int port=8000;
-    private HashMap<String, Pair<InetSocketAddress,Integer>> client_info;    //注册客户端信息
+    final int port = 8000;
+    private HashMap<String, Pair<InetSocketAddress,Integer>> clients_info;    //注册客户端信息
 
-    info_server() throws IOException {
-        client_info=new HashMap<>();
+    info_server() {
+        clients_info = new HashMap<>();
     }
 
-    public boolean sign_up(String user,InetSocketAddress address, int port){        //注册时
-    /*  Iterator<Map.Entry<String, InetSocketAddress>> iterator=client_info.entrySet().iterator();
-        while(iterator.hasNext()){
-            Map.Entry<String, InetSocketAddress> entry = iterator.next();
-            if (entry.getKey()==user){
-                System.out.println("注册失败，该用户名已被注册。");
-                return false;
-            }
+    public boolean Sign_Up(String user,InetSocketAddress address, int port){        //注册时
+        String msg;
+        boolean occupied;
+
+        occupied = clients_info.containsKey(user);
+        if (!occupied) {
+            clients_info.put(user, new Pair<>(address,port));
+            msg = "New User Signed";
+        } else {
+            msg = "User Name Occupied";
         }
-        client_info.put(user,address);
-        System.out.println("注册成功。");
-        return true;*/
 
-        boolean occuad = client_info.containsValue(user);
-        if (!occuad) {
-            client_info.put(user,new Pair<InetSocketAddress,Integer>(address,port));
-        }
-        return occuad;
-
-
-
+        System.out.println(msg);
+        return !occupied;
     }
 
-    public boolean is_in_info(String user,InetSocketAddress address){       //登录判断该用户名和ip是否以被注册
-   /*     Iterator<Map.Entry<String, InetSocketAddress>> iterator=client_info.entrySet().iterator();
-        while(iterator.hasNext()){
-            Map.Entry<String, InetSocketAddress> entry = iterator.next();
-            if (entry.getKey()==user){
-                if(entry.getValue()==address){
-                    return true;
+    public boolean Login_Check(String user, InetSocketAddress address, int port) {
+        if (!clients_info.containsKey(user)) {
+            return false;
+        } else {
+            clients_info.forEach((s, inetSocketAddressIntegerPair) -> {
+                if (s.equals(user)) {
+                    inetSocketAddressIntegerPair.s
                 }
-            }
+            });
         }
-        return false;*/
-
-
     }
 
+    public void On_Service()throws Exception{
+        System.out.println("Server Actvated");
+        try {
+            DatagramSocket socket = new DatagramSocket(port);
+        } catch (IOException e) {
+            System.out.println("ERROR::SOCKET_INIT_FAILED");
+            System.exit(1);
+        }
 
-
-
-
-    public void server()throws Exception{
-        System.out.println("信息服务器已启动.....");
-        DatagramSocket socket = new DatagramSocket(port);
         while(true){
             DatagramPacket request = new DatagramPacket(new byte[12], 12);
             socket.receive(request);
-
-
 
         }
     }
